2fbd9961244e4da6335ba8a07d3a955cf8c0acc7
cdnss <32522595+cdnss@users.noreply.github.com>
2025-05-02 18:57:12 +0700

    Update deno.ts

--- deno.ts
+++ deno.ts
@@ -1,9 +1,9 @@
-// script_proxy_iframe.ts
+// deno.ts
 import { serve } from "https://deno.land/std@0.224.0/http/server.ts";
 import * as cheerio from 'npm:cheerio'; // Impor Cheerio dari npm
 import { jq } from "./jq.ts";
 import { postDataToApi } from "./post.ts"; // Import fungsi postDataToApi dari post.ts
-import { processHtml } from "./xhtml.ts
+import { processHtml } from "./xhtml.ts";
 // HTML sederhana untuk halaman home dengan Bootstrap 5 dari CDN
 const homeHtml = await Deno.readTextFile('./index.html');
 
